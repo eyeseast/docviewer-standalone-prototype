e867ec207b5d21868c05db5da8c692b22d3ece41
Chris Amico <eyeseast@gmail.com>
2023-09-18 21:46:18 -0400

    Get all singular assets

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -4,11 +4,16 @@ This is a directory module in case I need to split things into files later.
 
 These should be saved in a pattern that matches the original API.
 """
+import json
 from pathlib import Path
+from urllib.parse import urljoin
+
+import httpx
 from documentcloud import DocumentCloud, Document
 
 ROOT = Path(__file__).parent.parent
-DOCUMENTS = ROOT / "documents"
+ASSETS = ROOT / "assets"
+DOCUMENTS = ASSETS / "documents"
 
 EXPAND = [
     "user",
@@ -22,18 +27,43 @@ EXPAND = [
 
 def document(client: DocumentCloud, id: int):
     "Download JSON data for a single document"
+    url = urljoin(client.base_uri, f"{client.documents.api_path}/{id}.json")
+    params = {"expand": EXPAND}
+
+    output = DOCUMENTS / f"{id}.json"
+    resp = httpx.get(url, params=params)
+
+    resp.raise_for_status()
+
+    # save our data
+    output.parent.mkdir(parents=True, exist_ok=True)
+    output.write_bytes(resp.content)
 
 
 def pdf(doc: Document):
     "Download the original PDF to documents/<id>/<slug>.pdf"
+    output = DOCUMENTS / str(doc.id) / f"{doc.slug}.pdf"
+
+    # save our data
+    output.parent.mkdir(parents=True, exist_ok=True)
+    output.write_bytes(doc.pdf)
 
 
 def full_text(doc: Document):
     "Download text extracted from a single document to documents/<id>/<slug>.txt"
+    output = DOCUMENTS / str(doc.id) / f"{doc.slug}.txt"
+
+    # save our data
+    output.parent.mkdir(parents=True, exist_ok=True)
+    output.write_text(doc.full_text)
 
 
-def text_json(doc: Document):
+def json_text(doc: Document):
     "Download text JSON for a single document to documents/<id>/<slug>.txt.json"
+    output = DOCUMENTS / str(doc.id) / f"{doc.slug}.txt.json"
+
+    output.parent.mkdir(parents=True, exist_ok=True)
+    output.write_text(json.dumps(doc.json_text, indent=2))
 
 
 def page_text(doc: Document):
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 """
 Fetch all assets required to embed a DocumentCloud viewer that can work offline.
 """
 
 from documentcloud.addon import AddOn
+
+import fetch
 
 
 class ExportViewer(AddOn):
@@ -10,23 +13,21 @@ class ExportViewer(AddOn):
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
 
         # add a hello note to the first page of each selected document
         for document in self.get_documents():
             # get_documents will iterate through all documents efficiently,
             # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
+            self.set_message(f"Fetching assets for document: {document.title}")
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            # fetch all the things, maybe someday this is async
+            fetch.document(self.client, document.id)
+            fetch.pdf(document)
+            fetch.full_text(document)
+            fetch.json_text(document)
+            fetch.page_text(document)
+            fetch.page_positions(document)
+            fetch.images(document)
 
 
 if __name__ == "__main__":
