50d88826510e8df8e583add3350dd6e2a64b0105
Chris Amico <eyeseast@gmail.com>
2023-09-19 10:12:57 -0400

    Download page assets

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -5,6 +5,7 @@ This is a directory module in case I need to split things into files later.
 These should be saved in a pattern that matches the original API.
 """
 import json
+from itertools import chain
 from pathlib import Path
 from urllib.parse import urljoin
 
@@ -27,6 +28,7 @@ EXPAND = [
 
 def document(client: DocumentCloud, id: int):
     "Download JSON data for a single document"
+    print("Downloading document data")
     url = urljoin(client.base_uri, f"{client.documents.api_path}/{id}.json")
     params = {"expand": EXPAND}
 
@@ -42,6 +44,7 @@ def document(client: DocumentCloud, id: int):
 
 def pdf(doc: Document):
     "Download the original PDF to documents/<id>/<slug>.pdf"
+    print("Downloading PDF")
     output = DOCUMENTS / str(doc.id) / f"{doc.slug}.pdf"
 
     # save our data
@@ -51,6 +54,7 @@ def pdf(doc: Document):
 
 def full_text(doc: Document):
     "Download text extracted from a single document to documents/<id>/<slug>.txt"
+    print("Downloading full text")
     output = DOCUMENTS / str(doc.id) / f"{doc.slug}.txt"
 
     # save our data
@@ -60,6 +64,7 @@ def full_text(doc: Document):
 
 def json_text(doc: Document):
     "Download text JSON for a single document to documents/<id>/<slug>.txt.json"
+    print("Downloading JSON text.")
     output = DOCUMENTS / str(doc.id) / f"{doc.slug}.txt.json"
 
     output.parent.mkdir(parents=True, exist_ok=True)
@@ -71,6 +76,14 @@ def page_text(doc: Document):
     Download page text for all pages in a document.
     Save results to documents/<id>/pages/<slug>-p<page number>.txt
     """
+    print("Downloading page text.")
+    output_dir = DOCUMENTS / str(doc.id) / "pages"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for i in range(1, doc.pages + 1):
+        output = output_dir / f"{doc.slug}-p{i}.txt"
+        text = doc.get_page_text(i)
+        output.write_text(text)
 
 
 def page_positions(doc: Document):
@@ -78,10 +91,36 @@ def page_positions(doc: Document):
     Download page positions for all pages in a document.
     Save results to documents/<id>/pages/<slug>-p<page number>.position.json
     """
+    print("Downloading page positions.")
+    output_dir = DOCUMENTS / str(doc.id) / "pages"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for i in range(1, doc.pages + 1):
+        output = output_dir / f"{doc.slug}-p{i}.position.json"
+        data = doc.get_page_position_json(i)
+        output.write_text(json.dumps(data, indent=2))
 
 
 def images(doc: Document):
     """
-    Download all imags of document pages.
+    Download all images of document pages.
     Save results to documents/<id>/pages/<slug>-p<page number>-<size>.gif
     """
+    print("Downloading page images.")
+    output_dir = DOCUMENTS / str(doc.id) / "pages"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    thumbnail = doc.get_thumbnail_image_url_list()
+    small = doc.get_small_image_url_list()
+    normal = doc.get_normal_image_url_list()
+    large = doc.get_large_image_url_list()
+
+    # download all the images, one by one, optimize later
+    with httpx.Client() as http:
+        for url in chain(thumbnail, small, normal, large):
+            resp = http.get(url)
+            resp.raise_for_status()
+
+            name = Path(url).name
+            output = output_dir / name
+            output.write_bytes(resp.content)
